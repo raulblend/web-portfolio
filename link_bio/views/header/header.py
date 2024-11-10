b20d289fa00f1549a9776091cdfc87cc90eb221e
raulblend <raulespinozavp@gmail.com>
2024-11-10 18:50:25 -0500

    minor changes

--- link_bio/views/header/header.py
+++ link_bio/views/header/header.py
@@ -21,7 +21,7 @@ def header() -> rx.Component:
         rx.text("Devops Engineer", size="6",
                 color= TextColor.BODY.value),
         rx.text("""I am a technology enthusiast with hands-on experience in DevOps, infrastructure automation, Bash scripting, and languages like Golang and Python. I hold the Azure Fundamentals (AZ-900) certification and have worked on projects that strengthen my skills in DevOps, Jenkins, cloud automation, and Bash scripting. 
-                I am seeking an opportunity in a dynamic team where I can continue developing as a DevOps Engineer and contribute innovative solutions that optimize automation and efficiency.""",
+                I am seeking an opportunity in a dynamic team where I can continue developing as a DevOps Engineer and contribute innovative solutions that optimize automation and efficiency!!!""",
                 color= TextColor.BODY.value),
                 padding="1em"
         ),
